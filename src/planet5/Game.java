d620264b49d2667396f76468ddce6819453d899b
Andrew Amis <atamiser@gmail.com>
2012-07-17 13:39:06 -0700

    Added the primary Game class.

---
+++ src/planet5/Game.java
@@ -0,0 +1,23 @@
+package planet5;
+
+import processing.core.PApplet;
+
+public class Game extends PApplet {
+
+	/**
+	 * Make Eclipse happy.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public static void main(String[] args) {
+		PApplet.main(new String[] {"planet5.Game" });
+	}
+}
