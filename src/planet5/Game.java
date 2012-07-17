9c9cf04960e088757ecf312e03ff4a6c6fbc7620
Andrew Amis <atamiser@gmail.com>
2012-07-17 13:58:38 -0700

    Added Consolas font.
    Also added a sinusoidal background. See Game.java for info on how to
    load and use fonts. Put fonts in the data directory.

--- src/planet5/Game.java
+++ src/planet5/Game.java
@@ -1,20 +1,25 @@
 package planet5;
 
-import processing.core.PApplet;
+import processing.core.*;
 
 public class Game extends PApplet {
-
+	public int ticks = 0;
+	public PFont consolas16;
 	/**
 	 * Make Eclipse happy.
 	 */
 	private static final long serialVersionUID = 1L;
 
 	public void setup() {
-		
+		consolas16 = loadFont("Consolas-32.vlw");
+		textFont(consolas16, 32);
 	}
 	
 	public void draw() {
+		ticks++;
+		background(map(sin((float) (ticks*0.1)), -1, 1, 0, 255));
 		
+		text("This is a test", 0, 0, 100, 100);
 	}
 	
 	public static void main(String[] args) {
