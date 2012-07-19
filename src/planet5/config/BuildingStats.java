fbabcb0de413a2e2b77999f97c7eafc2503c2a37
Andrew Amis <atamiser@gmail.com>
2012-07-19 14:05:50 -0700

    Added building stats as static final

---
+++ src/planet5/config/BuildingStats.java
@@ -0,0 +1,84 @@
+package planet5.config;
+
+public final class BuildingStats {
+
+	
+	// Main Base
+	static float base_health = 1500f;
+	static float base_cost = 0;
+	static float base_rows = 3;
+	static float base_cols = 3;
+	static float base_light = 8;
+	static float base_capacitance = 4800;
+	static float base_energy_gen = 20;
+	static float base_draw = 0;
+	
+	// Relay
+	static float relay_health = 100;
+	static float relay_cost = 20;
+	static float relay_rows = 1;
+	static float relay_cols = 1;
+	static float relay_light = 2;
+	static float relay_capacitance = 0;
+	static float relay_energy_gen = 0;
+	static float relay_draw = 0;
+	
+	// Solar Farm
+	static float farm_health = 100;
+	static float farm_cost = 30;
+	static float farm_rows = 1;
+	static float farm_cols = 1;
+	static float farm_light = 2;
+	static float farm_capacitance = 0;
+	static float farm_energy_gen = 50;
+	static float farm_draw = 0;
+
+
+	
+	// Capacitor
+	static float capacitor_health = 100;
+	static float capacitor_cost = 100;
+	static float capacitor_rows = 1;
+	static float capacitor_cols = 1;
+	static float capacitor_light = 2;
+	static float capacitor_capacitance = 500;
+	static float capacitor_energy_gen = 0;
+	static float capacitor_draw = 0;
+
+	
+	// Science Lab
+	static float lab_health = 500;
+	static float lab_cost = 500;
+	static float lab_rows = 2;
+	static float lab_cols = 2;
+	static float lab_light = 8;
+	static float lab_capacitance = 0;
+	static float lab_energy_gen = 0;
+	static float lab_draw = 10;
+
+	
+	// Laser Turret
+	static float laser_health = 500;
+	static float laser_cost = 350;
+	static float laser_rows = 1;
+	static float laser_cols = 1;
+	static float laser_light = 3;
+	static float laser_capacitance = 0;
+	static float laser_energy_gen = 0;
+	static float laser_damage = 4;
+	static float laser_draw = 10;
+
+	
+	// Mortar
+	static float mortar_health = 350;
+	static float mortar_cost = 500;
+	static float mortar_rows = 2;
+	static float mortar_cols = 2;
+	static float mortar_light = 2;
+	static float mortar_capacitance = 0;
+	static float mortar_energy_gen = 0;
+	static float mortar_draw = 4;
+	static float mortar_fire_cost = 20;
+	static float mortar_damage = 100;
+
+}
