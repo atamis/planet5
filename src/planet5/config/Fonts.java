78c8e53922cccaf7f87bdc0dd7dc74e60cc34b91
Andrew Amis <atamiser@gmail.com>
2012-07-31 10:23:03 -0700

    Added consolas at sizes 8 and 96. Added them to the font class. Also added some TODOs.

--- src/planet5/config/Fonts.java
+++ src/planet5/config/Fonts.java
@@ -4,11 +4,15 @@ import processing.core.PApplet;
 import processing.core.PFont;
 
 public final class Fonts {
+	public static PFont consolas8;
 	public static PFont consolas16;
 	public static PFont consolas32;
+	public static PFont consolas96;
 	
 	public static void load(PApplet applet) {
+		consolas8 = applet.loadFont("Consolas-8.vlw");
 		consolas16 = applet.loadFont("Consolas-16.vlw");
 		consolas32 = applet.loadFont("Consolas-32.vlw");
+		consolas96 = applet.loadFont("Consolas-96.vlw");
 	}
 }
